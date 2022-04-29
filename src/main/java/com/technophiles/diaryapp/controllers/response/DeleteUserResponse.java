c5bec10908e5fad2b4224da23f934d45341038fe
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-29 06:00:46 +0100

    my diary app

---
+++ src/main/java/com/technophiles/diaryapp/controllers/response/DeleteUserResponse.java
@@ -0,0 +1,10 @@
+package com.technophiles.diaryapp.controllers.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteUserResponse {
+    private String message;
+}
