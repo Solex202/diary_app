d1948a742e87a964d5042cba6de9437c1c6db319
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-29 04:40:15 +0100

    my diary app

---
+++ src/main/java/com/technophiles/diaryapp/controllers/response/CreateAccountResponse.java
@@ -0,0 +1,11 @@
+package com.technophiles.diaryapp.controllers.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccountResponse {
+
+    private String message;
+}
